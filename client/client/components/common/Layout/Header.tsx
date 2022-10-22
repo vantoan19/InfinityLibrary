caca4ae8747b8ffb8ea2dbac9471e08d90af64fb
Nguyen Viet Minh Hieu <ku8rla@inf.elte.hu>
2022-10-22 15:20:49 +0200

    Add svg from assets

--- client/client/components/common/Layout/Header.tsx
+++ client/client/components/common/Layout/Header.tsx
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from "react-native"
+import Logo from "../../../assets/logo.svg"
 
 export default function Header() {
   return (
     <View style={styles.container}>
       <Text>asda</Text>
+      <Logo />
     </View>
   )
 }
