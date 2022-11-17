e154a9c4ad6809b6f26e5fc434e31d33882ca964
Nguyen Hoai Nam <eko2gg@inf.elte.hu>
2022-11-17 12:55:29 +0100

    add delete api for books

--- server/server/core/db/database.py
+++ server/server/core/db/database.py
@@ -16,6 +16,6 @@ SQLALCHEMY_DATABASE_URL = (
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
 
 Base = declarative_base()
