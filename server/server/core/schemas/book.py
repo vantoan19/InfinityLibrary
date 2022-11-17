c4a93d9dad007be5d6acf7db944504054b888180
Nguyen Hoai Nam <eko2gg@inf.elte.hu>
2022-11-17 23:24:04 +0100

    addition information for Book (Location, condition, views)

--- server/server/core/schemas/book.py
+++ server/server/core/schemas/book.py
@@ -2,7 +2,7 @@ from fastapi import UploadFile
 from dataclasses import dataclass
 from pydantic import BaseModel
 from enum import Enum
-from typing import List
+from typing import List, Any
 from server.core.models import BookStatus
 from .user import User
 
@@ -33,9 +33,11 @@ class Book(BookBase):
     title: str
     description: str
     published_year: int
-    user: User
+    location: str = "Budapest, Hungary"
     book_condition: str = "Newly open"
-    view: int = 0
+    views: int = 52
+    image_covers: List[Any] = [{"uri":"https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"},{"uri":"https://images.unsplash.com/photo-1495640388908-05fa85288e61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"}]
+
 
     class Config:
         orm_mode = True
